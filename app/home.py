dc034e245510f0f89d28f3725d3454029bea4aef
ja.torrespinosa@gmail.com <ja.torrespinosa@gmail.com>
2023-09-17 20:39:07 +0200

    Update pages, comments and documentation

--- app/home.py
+++ app/home.py
@@ -1,71 +1,77 @@
-# --- TFM - TorresEspinosa,JoseAntonio ---
+# ----------------------------------------------------------------------------
+#   File to init the tfm streamlit App, deploy with 'streamlit run app/home.py'.
+#   Torres Espinosa, Jose Antonio.
+# ----------------------------------------------------------------------------
+
+# Dependencies
 import os
 import sys
 import streamlit as st
-# import plotly.express as px
-import time
-
-from sklearn.model_selection import train_test_split
-# from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-
+# Own Modules
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append('.')
 sys.path.append('..')
 sys.path.append('tfm_predictor')
-
 from utils.opendata import OpenData
 
 class App():
     def __init__(self):
-        self.opd = OpenData()
+        """App Constructor.
+        """
+        self.opd = OpenData()  # Data provider instance
         self.data = ''
 
-   
     # --------------------------
     # -------- MAIN ------------
     # --------------------------
-    def main(self):
-        # Título Pestaña
+    def main(self):  # Home page
+        # Tab title
         st.set_page_config(page_title="TFM", layout="wide")  # , page_icon="blue-flower.ico"
 
-        opciones = ('Almeria','Cadiz','Cordoba','Granada',
-                        'Huelva','Jaen','Malaga','Sevilla')
-        years = (2023,2022,2021,2020,2019)
         # --- SideBar ---
         with st.sidebar:
             st.markdown('''# TFM - Torres Espinosa''')
 
-    # --- Página central ---
-        # Título
+        # --- Principal section ---
+        # Title
         st.title("TFM - Wheather Predictor")
-        # Descripción
-        st.write("Machine Learning App for predict wheather of Andalucía.\
-                 Uses Aemet data through its Open Data Api.")
+        # Description
+        st.write("""Its a Machine learning App to predict wheather in Andalusia. 
+            Uses Aemet data through its Open Data Api. 
+            Requests the stations data and processes it with internal code.
+            This app shows data graphs, analysis graps and accuracy results of 
+            Machine Learning models.
+            This app runs with streamlit and the machine learning section uses 
+            scikit-learn.
+            Developed by Torres Espinosa, Jose Antonio.""")
 
-        st.title('Soruce Data:')
-        st.subheader('Data Location:')
-        
-        c1, c2, c3 = st.columns(3)
+        st.title('Data Location:')
+        c1, c2, c3 = st.columns(3)  # Creates 3 columns on page
+        # Selectbox to choose year, station and province.
         with c1:
-            # Selectbox para elegir que datos ver
-            provincia = st.selectbox('Province:', opciones, 6)
-            stations = self.opd.get_stations().query(
-                f'provincia == "{(provincia.upper())}"')
+            options = ('Almeria','Cadiz','Cordoba','Granada',  # for province
+                        'Huelva','Jaen','Malaga','Sevilla')
+            province = st.selectbox('Province:', options, 6)
+            stations = self.opd.get_stations().query(  # gets province stations
+                f'provincia == "{(province.upper())}"')
         with c2:
-            idema_name = st.selectbox('Station:', stations[['nombre']],
+            idema_name = st.selectbox('Station:', stations[['nombre']],  # choose station
                                       len(stations)//2)
-            idema = stations.query(f'nombre == "{idema_name}"'
-                                                )['indicativo'].values[0]
+            idema = stations.query(f'nombre == "{idema_name}"')\
+                                  ['indicativo'].values[0]  # gets idema
         with c3:
-            year = st.selectbox('Year:', years)
+            years = (2023,2022,2021,2020,2019)
+            year = st.selectbox('Year:', years)  # choose year
             self.opd.set_idema(idema)
-            self.data = self.opd.get_year(year, source=True)
+            self.data = self.opd.get_year(year, source=True)  # Obtains source data
+        # Shows source data
+        st.subheader('Source Data:')
+        st.write(self.data)
+        # Shows formatted data
+        st.subheader('Formatted Data:')
+        st.write(self.opd.get_year(year))  # Obtains formatted data
 
-        st.write(self.data)  
 
-
-if __name__ == '__main__':
+if __name__ == '__main__':  # Run the application
     main = App()
     main.main()
--- app/pages/1_Graphics.py
+++ app/pages/1_Graphics.py
@@ -1,9 +1,16 @@
+# ----------------------------------------------------------------------------
+#   File to see the data graphics.
+#   Torres Espinosa, Jose Antonio.
+# ----------------------------------------------------------------------------
+
+# Dependencies
 import os
 import sys
 import matplotlib.pyplot as plt
 import pandas as pd
 import streamlit as st
 
+# Own Modules
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append('.')
 sys.path.append('..')
@@ -12,10 +19,17 @@ from utils.opendata import OpenData
 from app.home import App
 
 # opd = OpenData()
-def view_temp(df):
-    if not isinstance(df, str):
+def view_graphs(df):
+    """Creates graphics for temperature, wind and pressure data.
+
+    :param df: formatted data for opendata() one or several years.
+    :type df: DataFrame
+    """
+    if not isinstance(df, str):  # If contains data
         plt.rcParams.update({'font.size': 20})
         fig, ax = plt.subplots(nrows=4, figsize=(23,23))
+
+        # - Temperature -
         ax[0].grid()
         ax[0].bar(df.index, df["rain"]*4, label="Raining Day")
         ax[0].plot(df.index, df["tmed"], label="Media", color='green')
@@ -26,6 +40,7 @@ def view_temp(df):
         ax[0].set_title("Temperaturas diarias")
         ax[0].legend(ncols=2)
 
+        # - Wind -
         ax[1].grid()
         ax[1].plot(df.index, df["velmedia"], label="Velocidad media", color='orange')
         ax[1].plot(df.index, df["racha"], label="Racha máxima", color='purple')
@@ -38,12 +53,12 @@ def view_temp(df):
         ax[2].grid()
         ax[2].plot(df.index, df["dir"], label="Dirección de la racha máx.", color='cyan')
         ax[2].bar(df.index, df["rain"]*4, label="Rain Day")
-
         #ax[2].set_xlabel("Fecha")
         ax[2].set_ylabel("º (grados)")
         ax[2].set_title("Dirección")
         ax[2].legend(loc=2, framealpha=.4)
 
+        # - Pressure -
         ax[3].grid()
         ax[3].plot(df.index, df["presMax"], label="Presión Máxima", color='orange')
         ax[3].plot(df.index, df["presMin"], label="Presión Mínima", color='purple')
@@ -53,43 +68,40 @@ def view_temp(df):
         ax[3].set_title("Presiones")
         ax[3].legend()
 
-        st.pyplot(fig)
+        st.pyplot(fig)  # Shows subplots
     else:
-        st.write(df)
+        st.write(df)  # If not cotains data
 
 
 
-def main():
-    st.set_page_config(layout="wide")
-
+def main():  # Graphics page
+    st.set_page_config(layout="wide")  # Set wide option
+    # - sidebar -
     with st.sidebar:
-        opciones = ('Almeria','Cadiz','Cordoba','Granada',
-                        'Huelva','Jaen','Malaga','Sevilla')
-        years = (2023,2022,2021,2020,2019)
-
         st.markdown('''# TFM - Torres Espinosa\n ### Data Location:''')
         
-        # Selectbox para elegir que datos ver
-        provincia = st.selectbox("Province: ", opciones, 6)
-        stations = app.opd.get_stations().query(
-            f'provincia == "{(provincia.upper())}"')
-        
+        # Selectbox to choose year, station and province.
+        options = ('Almeria','Cadiz','Cordoba','Granada',
+                        'Huelva','Jaen','Malaga','Sevilla')
+        years = (2023,2022,2021,2020,2019)
+        province = st.selectbox("Province: ", options, 6)  # choose province
+        stations = app.opd.get_stations().query(  # obtains province stations
+            f'provincia == "{(province.upper())}"')
         idema_name = st.selectbox( "Station: ", stations[['nombre']],
                                   len(stations)//2)
-        idema = stations.query(f'nombre == "{idema_name}"'
-                                            )['indicativo'].values[0]
-        year = st.selectbox('Year:', years)
+        idema = stations.query(f'nombre == "{idema_name}"')\
+                               ['indicativo'].values[0]  # gets idema
+        year = st.selectbox('Year:', years)  # choose year
         app.opd.set_idema(idema)
-        app.data = app.opd.get_year(year)
+        app.data = app.opd.get_year(year)  # gets formatted data
 
-    # Título Pestaña
+    # Tab title
     st.title('Graphics')
-
-    st.write(app.data)
     
-    view_temp(app.data)
+    view_graphs(app.data)  # Show graphics of data
 
-if __name__ == '__main__':
+
+if __name__ == '__main__':  # Run the application
     app = App()
     main()
 
